f963c70032b3bf40e6e842d1d6f1169ec3ba6aa6
MonsterD173 <91997877+MonsterD173@users.noreply.github.com>
2026-07-15 21:35:10 +0700

    Create src/data/__init__.py with module exports

---
+++ src/data/__init__.py
@@ -0,0 +1,13 @@
+from .dataset import (
+    get_data,
+    print_statistics,
+    load_cora_data,
+    print_dataset_stats,
+)
+
+__all__ = [
+    'get_data',
+    'print_statistics',
+    'load_cora_data',
+    'print_dataset_stats',
+]
